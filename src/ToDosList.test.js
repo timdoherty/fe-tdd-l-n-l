bffc9421b5c9b119c870c4db8645ec1ee36fa4d5
Tim Doherty <tim.doherty@procore.com>
2018-07-09 12:40:57 -0700

    pull in BDD assertions for ToDosList spec

---
+++ src/ToDosList.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import ToDosList from './ToDosList';
+import ToDoItem from './ToDoItem';
+import todos from './todos';
+
+function makeTodos(howMany) {
+  for (let i = 0; i < howMany; i++) {
+    todos.add(`todo${i}`);
+  }
+}
+
+describe('<ToDosList/>', () => {
+  beforeEach(() => {
+    todos.clearAll();
+  });
+
+  describe('Shallow Rendering', () => {
+    it('renders a ToDoItem for each todo', () => {
+    });
+
+    it('adds a todo', () => {
+    });
+
+    it('completes a todo', () => {
+    });
+
+    it('shows only active todos', () => {
+    });
+
+    it('shows only completed todos', () => {
+    });
+  });
+});
