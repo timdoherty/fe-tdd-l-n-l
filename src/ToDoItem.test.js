9cb571bda8d853c41afcb24fb79bb862b601d90e
Tim Doherty <tim.doherty@procore.com>
2018-07-09 10:18:52 -0700

    BDD assertions for ToDoItem

---
+++ src/ToDoItem.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Checkbox } from '@procore/core-react';
+
+import ToDoItem from './ToDoItem';
+
+describe('<ToDoItem/>', () => {
+  describe('Shallow Rendering', () => {
+    it('shows the todo text', () => {
+    });
+
+    it('has a completed indicator', () => {
+    });
+
+    it('calls back when completed is toggled', () => {
+    });
+  });
+});
