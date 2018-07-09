32196d15091d2d1fee56fb8d01cbe620df65940c
Tim Doherty <tim.doherty@procore.com>
2018-07-09 10:08:29 -0700

    BDD style initial specs

--- src/todos.test.js
+++ src/todos.test.js
@@ -1,1 +1,24 @@
+import todos from './todos';
 
+describe('todos', () => {
+  beforeEach(() => {
+  });
+
+  it('adds a todo to the list', () => {
+  });
+
+  it('completes a todo from the list', () => {
+  });
+
+  it('clears completed todos from the list', () => {
+  });
+
+  it('lists all todos', () => {
+  });
+
+  it('lists active todos', () => {
+  });
+
+  it('lists completed todos', () => {
+  });
+});
