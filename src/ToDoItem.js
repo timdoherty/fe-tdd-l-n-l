02db9168943e1de5a85bc1d3c6768b6584ec79b0
Tim Doherty <tim.doherty@procore.com>
2018-07-10 08:14:37 -0700

    revert markup, fix test

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { Checkbox, Card } from '@procore/core-react';
+import { Checkbox } from '@procore/core-react';
 
 export default ({ todo, completed = false, onCompleted = () => {} }) => (
-  <Card style={{ padding: '10px' }}>
+  <div>
     <Checkbox
       checked={completed}
       onClick={() => onCompleted({ todo, completed: !completed })}
     >
       {todo}
     </Checkbox>
-  </Card>
+  </div>
 )
